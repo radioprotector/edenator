6361d8033235b7c5b2b845b1824a9a3394a79873
Cate Stock <radioprotector@users.noreply.github.com>
2021-12-27 18:19:31 -0500

    start to define alternate themes

--- src/Themes.ts
+++ src/Themes.ts
@@ -1,31 +1,86 @@
 import * as THREE from 'three';
 
+/**
+ * A theme to use for the visualizer.
+ */
 export interface Theme {
+  /**
+   * Contains theming information relevant to the BassTunnel component.
+   */
   bass: {
+    /**
+     * The color to use for wireframes that form the "tunnel".
+     */
     wireframeColor: THREE.Color;
 
+    /**
+     * The color to use for panels that fill different parts of the tunnel.
+     */
     panelColor: THREE.Color;
   },
+
+  /**
+   * Contains theming information relevant to the BeatQueue component.
+   */
   beat: {
+    /**
+     * The color to use for incoming beat peaks.
+     */
     color: THREE.Color;
   },
+
+  /**
+   * Contains theming information relevant to the TrebleQueue component.
+   */
   treble: {
+    /**
+     * The sprite color to use for incoming treble peaks.
+     */
     spriteColor: THREE.Color;
 
+    /**
+     * The path to the sprite texture to use for incoming treble peaks.
+     */
     spriteTexture: string;
 
+    /**
+     * The light color to use for incoming treble peaks.
+     */
     lightColor: THREE.Color;
   },
+
+  /**
+   * Contains theming information relevant to the FrequencyGrid component.
+   */
   frequencyGrid: {
+    /**
+     * The color to use for drawing frequency lines.
+     */
     lineColor: THREE.Color;
   },
+
+  /**
+   * Contains theming information relevant to the overall visualizer and BackgroundManager component.
+   */
   background: {
+    /**
+     * The color to use for the sun.
+     */
     sunColor: THREE.Color;
 
+    /**
+     * The color to use for the "burst" lines that rotate around the sun.
+     */
     burstLineColor: THREE.Color;
 
+    /**
+     * The standard color to use for stars in the background.
+     */
     starColor: THREE.Color;
 
+    /**
+     * The color to use for "flashed" stars in the background.
+     */
     starFlashColor: THREE.Color;
   }
 }
@@ -52,4 +107,28 @@ export const defaultTheme: Theme = {
     starColor: new THREE.Color(0xaa66aa),
     starFlashColor: new THREE.Color(0xffffff)
   }
-}
+};
+
+export const pinkTheme: Theme = {
+  bass: {
+    wireframeColor: new THREE.Color(0xFF00E4),
+    panelColor: new THREE.Color(0xED50F1)
+  },
+  beat: {
+    color: new THREE.Color(0xFDB9FC)
+  },
+  treble: {
+    spriteColor: new THREE.Color(0xffdcdc),
+    spriteTexture: 'textures/extendring.png',
+    lightColor: new THREE.Color(0xffeeee)
+  },
+  frequencyGrid: {
+    lineColor: new THREE.Color(0xaa00aa)
+  },
+  background: {
+    sunColor: new THREE.Color(0xffcc55),
+    burstLineColor: new THREE.Color(0xffdcdc),
+    starColor: new THREE.Color(0xaa00aa),
+    starFlashColor: new THREE.Color(0xffffff)
+  }
+};
