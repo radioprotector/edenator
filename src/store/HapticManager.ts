01fc635bd2f050acc6cad39ecc98a9c0a55d9bd1
Cate Stock <radioprotector@users.noreply.github.com>
2022-10-01 03:02:38 -0400

    Restructure/clean up internals of haptic manager

--- src/store/HapticManager.ts
+++ src/store/HapticManager.ts
@@ -1,24 +1,30 @@
+import { MathUtils } from 'three';
 import Lull from "./Lull";
 import Peak from "./Peak";
 
-// https://github.com/luser/gamepadtest
-
+/**
+ * Handles controller detection and haptic feedback
+ * {@see https://github.com/luser/gamepadtest}
+ */
 export class HapticManager {
 
+  /**
+   * The name of the window event bubbled when gamepads are connected or disconnected.
+   */
   public static readonly ChangeEventName: string = 'HapticGamepadsChanged';
 
   private gamepads: Gamepad[] = [];
 
+  /**
+   * Indicates whether or not haptic feedback is enabled.
+   */
   public isEnabled: boolean = false;
-
-  public get isInitialized(): boolean {
-    return this.managerInitialized;
-  }
 
   private managerInitialized: boolean = false;
 
-  // private scanGamepadsInterval: NodeJS.Timer | undefined;
-
+  /**
+   * Ensures that the haptic manager is initialized with appropriate controller detection.
+   */
   public ensureInitialized(): void {
     if (this.managerInitialized) {
       return;
@@ -29,9 +35,6 @@ export class HapticManager {
       window.addEventListener('gamepadconnected', this.handleGamepadConnected);
       window.addEventListener('gamepaddisconnected', this.handleGamepadDisconnected);
     }
-    // else if ('getGamepads' in navigator) {
-    //   this.scanGamepadsInterval = setInterval(this.scanGamepads, 500);
-    // }
 
     // Request vibration access if available, by making a dummy call
     if ('vibrate' in navigator) {
@@ -44,26 +47,22 @@ export class HapticManager {
     this.managerInitialized = true;
   }
 
+  /**
+   * Disposes the haptic manager.
+   */
   public dispose(): void {
     if ('GamepadEvent' in window) {
       window.removeEventListener('gamepadconnected', this.handleGamepadConnected);
       window.removeEventListener('gamepaddisconnected', this.handleGamepadDisconnected);
     }
-
-    // clearInterval(this.scanGamepadsInterval);
   }
-
-  // private scanGamepads = (): void => {
-  //   if ('getGamepads' in navigator) {
-  //     const gamepads = navigator.getGamepads();
-
-  //     for()
-  //   }
-  // };
 
   private handleGamepadConnected = (event: GamepadEvent): void => {
     const gamepad = event.gamepad;
-    console.trace('gamepad connected', event.gamepad);
+
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('gamepad connected', gamepad);
+    } 
 
     // Add this to the list if it has any actuators
     if (this.getActuatorsForGamepad(gamepad).length > 0) {
@@ -74,7 +73,9 @@ export class HapticManager {
   };
 
   private handleGamepadDisconnected = (event: GamepadEvent): void => {
-    console.trace('gamepad disconnected', event.gamepad);
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('gamepad disconnected', event.gamepad);
+    }
 
     // Remove from the array if available
     if (event.gamepad.index in this.gamepads) {
@@ -84,6 +85,10 @@ export class HapticManager {
     }
   };
 
+  /**
+   * Determines whether there is at least one gamepad that supports haptic feedback.
+   * @returns True if at least one gamepad supports haptic feedback; otherwise, false.
+   */
   public hasEligibleGamepad(): boolean {
     // See if there are any available gamepads
     for (const gamepad of this.gamepads) {
@@ -97,18 +102,27 @@ export class HapticManager {
     return false;
   }
 
+  /**
+   * "Plays" haptic feedback for the specified peak.
+   * @param peak {Peak} The peak.
+   */
   public playFeedback(peak: Peak): void {
     // Ensure feedback is enabled
     if (!this.isEnabled) {
       return;
     }
 
+    // Calculate duration and intensities
+    const durationMs = HapticManager.getFeedbackDurationMs(peak);
+    const strongMagnitude = MathUtils.clamp(peak.intensityNormalized, 0.75, 1.0);
+    const weakMagnitude = MathUtils.clamp(peak.intensity, 0.5, 1.0);
+
     // Look for enabled actuators
     let hasActuator = false;
 
     for (const gamepad of this.gamepads) {
       for (const gamepadActuator of this.getActuatorsForGamepad(gamepad)) {
-        this.playFeedbackForActuator(gamepadActuator, peak);
+        this.playFeedbackForActuator(gamepadActuator, durationMs, strongMagnitude, weakMagnitude);
         hasActuator = true;
       }
     }
@@ -117,82 +131,88 @@ export class HapticManager {
     if (!hasActuator) {
       if ('vibrate' in navigator) {
         try {
-          navigator.vibrate(HapticManager.getFeedbackDurationMs(peak));
+          navigator.vibrate(durationMs);
         }
         catch {}
       }
     }
   }
 
-  private playFeedbackForActuator(hapticActuator: GamepadHapticActuator, peak: Peak): void {
-    try {
-      const clampedIntensityNormalized = Math.min(1.0, Math.max(0.75, peak.intensityNormalized));
-      const clampedIntensity = Math.min(1.0, Math.max(0.5, peak.intensity));
-
-      // HACK: pulse and playEffect are not defined in GamepadHapticActuator typings
-      if ((hapticActuator as any).pulse) {
-        // https://developer.mozilla.org/en-US/docs/Web/API/GamepadHapticActuator/pulse
-        (hapticActuator as any).pulse(clampedIntensityNormalized, HapticManager.getFeedbackDurationMs(peak));
-      }
-      else if ((hapticActuator as any).playEffect)
-        // https://developer.mozilla.org/en-US/docs/Web/API/GamepadHapticActuator/playEffect
-        (hapticActuator as any).playEffect(hapticActuator.type, {
-          duration: HapticManager.getFeedbackDurationMs(peak),
-          startDelay: 0,
-          strongMagnitude: clampedIntensityNormalized,
-          weakMagnitude: clampedIntensity
-        });
-      }
-    catch {}
-  }
-
-  private static getFeedbackDurationMs(peak: Peak): number {
-    // Convert the peak into a milliseconds-based duration, double it (because these are pretty short), and assign a minimum of 100ms and a maximum of 2000ms
-    return Math.min(Math.max(Math.floor((peak.end - peak.time) * 2000), 100), 2000);
-  }
-
+  /**
+   * "Plays" haptic feedback for the specified lull.
+   * @param lull {Lull} The lull.
+   */
   public playLullFeedback(lull: Lull): void {
     // Ensure feedback is enabled
     if (!this.isEnabled) {
       return;
     }
 
+    // Calculate duration and intensities
+    const durationMs = HapticManager.getLullFeedbackDurationMs(lull);
+    const strongMagnitude = 0.5;
+    const weakMagnitude = 0.25;
+
     // Unlike peaks, we don't want to fall back to the vibration navigator
     for (const gamepad of this.gamepads) {
       for (const gamepadActuator of this.getActuatorsForGamepad(gamepad)) {
-        this.playLullFeedbackForActuator(gamepadActuator, lull);
+        this.playFeedbackForActuator(gamepadActuator, durationMs, strongMagnitude, weakMagnitude);
       }
     }
   }
 
-
-  private playLullFeedbackForActuator(hapticActuator: GamepadHapticActuator, lull: Lull): void {
+  /**
+   * "Plays" haptic feedback for the specified actuator.
+   * @param hapticActuator The haptic actuator to use.
+   * @param durationMs The duration, in milliseconds.
+   * @param strongMagnitude The strong vibration magnitude on a 0.0-1.0 scale.
+   * @param weakMagnitude The weak vibration magnitude on a 0.0-1.0 scale. Only supported for playEffect-based actuators.
+   */
+  private playFeedbackForActuator(hapticActuator: GamepadHapticActuator, durationMs: number, strongMagnitude: number, weakMagnitude: number): void {
     try {
-      const lullIntensity = 0.5;
-      const lullIntensityWeak = 0.25;
-
       // HACK: pulse and playEffect are not defined in GamepadHapticActuator typings
       if ((hapticActuator as any).pulse) {
         // https://developer.mozilla.org/en-US/docs/Web/API/GamepadHapticActuator/pulse
-        (hapticActuator as any).pulse(lullIntensity, HapticManager.getLullFeedbackDurationMs(lull));
+        (hapticActuator as any).pulse(strongMagnitude, durationMs);
       }
       else if ((hapticActuator as any).playEffect)
         // https://developer.mozilla.org/en-US/docs/Web/API/GamepadHapticActuator/playEffect
         (hapticActuator as any).playEffect(hapticActuator.type, {
-          duration: HapticManager.getLullFeedbackDurationMs(lull),
+          duration: durationMs,
           startDelay: 0,
-          strongMagnitude: lullIntensity,
-          weakMagnitude: lullIntensityWeak
+          strongMagnitude: strongMagnitude,
+          weakMagnitude: weakMagnitude
         });
       }
     catch {}
+  } 
+
+  /**
+   * Gets the duration of haptic feedback for the specified peak.
+   * @param peak The peak.
+   * @returns The corresponding duration, in milliseconds.
+   */
+  private static getFeedbackDurationMs(peak: Peak): number {
+    // Convert the peak into a milliseconds-based duration and double it (because these are pretty short).
+    // Furthermore, assign a minimum of 100ms and a maximum of 2000ms
+    return MathUtils.clamp(Math.floor((peak.end - peak.time) * 2000), 100, 2000);
   }
 
+  /**
+   * Gets the duration of haptic feedback for the specified lull.
+   * @param lull The lull.
+   * @returns The corresponding duration, in milliseconds.
+   */
   private static getLullFeedbackDurationMs(lull: Lull): number {
     // Cap the lull at 7 seconds
-    return Math.min(Math.floor(lull.duration * 1000), 7000);
+    return MathUtils.clamp(Math.floor(lull.duration * 1000), 500, 7000);
   }
 
+  /**
+   * Gets available haptic actuators for the provided gamepad.
+   * @param gamepad The gamepad.
+   * @returns A read-only array of haptic actuators for the gamepad, if any.
+   */
   private getActuatorsForGamepad(gamepad: Gamepad): readonly GamepadHapticActuator[] {
     // Skip over disconnected gamepads
     if (!gamepad || !gamepad.connected) {
