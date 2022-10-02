10ce26b57a5495a257d953b2deddc2d31a296888
Cate Stock <radioprotector@users.noreply.github.com>
2022-10-02 14:37:22 -0400

    Fix preload behavior for themes

--- src/components/ThemeCyclerButton.tsx
+++ src/components/ThemeCyclerButton.tsx
@@ -7,15 +7,21 @@ import { getNextTheme } from '../store/themes';
 import { getSceneryModelUrls } from '../store/scenery';
 
 function ThemeCyclerButton(): JSX.Element {
+  // Pull the current theme so this button is reactive (and can thus preload) based on it
+  const currentTheme = useStore(store => store.theme);
   const setStoreTheme = useStore(store => store.setTheme);
 
-  // Peek at the next theme and use that to pre-load models
-  const nextTheme = getNextTheme(useStore.getState().theme);
+  // Peek at the next theme and use that to pre-load models.
+  const nextTheme = getNextTheme(currentTheme);
   const nextThemeModels = getSceneryModelUrls(nextTheme.scenery.availableItems);
+  // if (process.env.NODE_ENV !== 'production' && nextThemeModels.length) {
+  //   // This message displays twice in strict mode because React double-renders
+  //   console.debug(`preloading models for ${nextTheme.name}`, nextThemeModels);
+  // }
   useLoader.preload(GLTFLoader, nextThemeModels);
 
   const cycleTheme = () => {
-    const nextTheme = getNextTheme(useStore.getState().theme);
+    const nextTheme = getNextTheme(currentTheme);
     
     if (process.env.NODE_ENV !== 'production') {
       console.debug(`switching to ${nextTheme.name} theme`);
